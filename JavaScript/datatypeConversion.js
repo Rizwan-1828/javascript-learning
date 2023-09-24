e5b7245d848a4a4e70bc4c54fd073f1bdef7ab7c
Rizwan._.1828 <84914713+Rizwan-1828@users.noreply.github.com>
2023-09-24 17:57:48 +0000

    typeConversion

---
+++ JavaScript/datatypeConversion.js
@@ -0,0 +1,27 @@
+let score = false
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+//  true => 1; false => 0
+
+let isLoggedIn = 0.0
+// console.log(typeof isLoggedIn);
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
+
+// 1,2,3..... => true , 0 => false 
+// "abc..." => true , "" => false 
+
+let number = 1234
+console.log(typeof number)
+
+let numberInString = String(number)
+console.log(typeof numberInString);
+console.log(numberInString);
