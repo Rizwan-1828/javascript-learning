ce01090a0b702c24a7c144226bec269c262286b9
Rizwan._.1828 <84914713+Rizwan-1828@users.noreply.github.com>
2023-10-07 18:16:41 +0000

    numbers and maths file

---
+++ JavaScript/numsAndMaths.js
@@ -0,0 +1,8 @@
+const firstNumber = 300
+console.log(firstNumber);
+
+const otherNumber = new Number(270.07912)
+console.log(otherNumber);
+
+console.log(otherNumber.toFixed(3));
+console.log(otherNumber.toPrecision());
